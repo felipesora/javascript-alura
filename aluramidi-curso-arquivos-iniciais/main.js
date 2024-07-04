b5743c2806cd3cdc74ca873f5ea14893533b7c1d
Felipe Ulson Sora <felipeusora@gmail.com>
2024-07-03 21:11:57 -0300

    avançando no js

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,20 @@
-document.querySelector('.tecla_pom');
+
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+// document.querySelector('.tecla_pom').onclick = tocaSomPom
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom
+listaDeTeclas[1].onclick = tocaSomClap
+listaDeTeclas[2].onclick = tocaSomTim
