da35f7d739f16b1ead3bd4c2acad1fd0f301106d
Felipe Ulson Sora <felipeusora@gmail.com>
2024-07-08 19:53:14 -0300

    projeto fokus finalizado

--- Fokus-projeto-base/script.js
+++ Fokus-projeto-base/script.js
@@ -9,9 +9,41 @@ const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 // Para add e remover classes
 const botoes = document.querySelectorAll('.app__card-button');
+// Botão iniciar Temporizador 
+const startPauseBt = document.querySelector('#start-pause');
+// Para adicionar Musica
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('sons/luna-rise-part-one.mp3');
+musica.loop = true;
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused){
+        musica.play();
+    } else {
+        musica.pause();
+    }
+});
+
+// áudios do temporizador
+const audioPlay = new Audio('sons/play.wav');
+const audioPausa = new Audio('sons/pause.mp3');
+const audioTempoFinalizado = new Audio('sons/beep.mp3')
+
+// mudar a palavra do botao começar
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
+// mudar imagem do botao começar 
+const iconIniciarOuPausarBt = document.querySelector('.app__card-primary-butto-icon');
+// printar o temporizador
+const tempoNaTela = document.querySelector('#timer');
+
+
+// temporizador
+let tempoDecorridoEmSegundos = 1500;
+let intervaloId = null
 
 // Eventos de clicks dos botões
 focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500;
     // html.setAttribute('data-contexto', 'foco');
     // banner.setAttribute('src', 'imagens/foco.png');
     // Deixando o código melhor com a Função alterarContexto()
@@ -20,17 +52,20 @@ focoBt.addEventListener('click', () => {
 });
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300;
     alterarContexto('descanso-curto')
     curtoBt.classList.add('active');
 });
 
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900;
     alterarContexto('descanso-longo')
     longoBt.classList.add('active');
 });
 
 // função para otimizar o código
 function alterarContexto(contexto) {
+    mostrarTempo();
     botoes.forEach(function (contexto){
         contexto.classList.remove('active');
     })
@@ -62,3 +97,43 @@ function alterarContexto(contexto) {
             break;
     }
 }
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0){
+        audioTempoFinalizado.play()
+        alert('Tempo Finalizado');
+        zerar()
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    mostrarTempo()
+};
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar(){
+    if (intervaloId){
+        audioPausa.play();
+        zerar()
+        return
+    }
+    audioPlay.play();
+    intervaloId = setInterval(contagemRegressiva, 1000)
+    iniciarOuPausarBt.textContent = "Pausar"
+    iconIniciarOuPausarBt.setAttribute('src', 'imagens/pause.png');
+};
+
+function zerar(){
+    clearInterval(intervaloId);
+    intervaloId = null
+    iniciarOuPausarBt.textContent = "Começar"
+    iconIniciarOuPausarBt.setAttribute('src', 'imagens/play_arrow.png');
+}
+
+function mostrarTempo () {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'})
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+mostrarTempo()
