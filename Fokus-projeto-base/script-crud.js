65d164b6d521a78c31155469a85ff67b5af7339e
Felipe Ulson Sora <felipeusora@gmail.com>
2024-07-09 20:32:41 -0300

    update js do Fokus

--- Fokus-projeto-base/script-crud.js
+++ Fokus-projeto-base/script-crud.js
@@ -3,12 +3,61 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textArea = document.querySelector('.app__form-textarea');
+const ulTarefas = document.querySelector('.app__section-task-list');
 
-const tarefas = []
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+
+function atualizarTarefas () {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas)) //guarda a lista de tarefas dentro de um localStorage
+}
+
+// o html está no fim da página
+function criarElementoTarefa(tarefa){
+    const li = document.createElement('li');
+    li.classList.add('app__section-task-list-item');
+
+    const svg = document.createElement('svg');
+    svg.innerHTML = `
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>
+    `
+
+    const paragrafo = document.createElement('p');
+    paragrafo.textContent = tarefa.descricao
+    paragrafo.classList.add('app__section-task-list-item-description');
+
+    const botao = document.createElement('button');
+    botao.classList.add('app_button-edit');
+
+    // botao de editar 
+    botao.onclick = () => {
+        //debugger -> ajudar a entender o erro
+        const novaDescricao = prompt("Qual é o novo nome da tarefa?");
+        console.log('Nova descrição da tarefa: ', novaDescricao)
+        if (novaDescricao) {
+            paragrafo.textContent = novaDescricao;
+            tarefa.descricao = novaDescricao
+            atualizarTarefas(); 
+        }
+        
+    }
+
+    const imagemBotao = document.createElement('img');
+    imagemBotao.setAttribute('src', 'imagens/edit.png');
+    botao.append(imagemBotao);
+
+    li.append(svg);
+    li.append(paragrafo);
+    li.append(botao);
+
+    return li;
+}
 
 btnAdicionarTarefa.addEventListener('click', () => {
     formAdicionarTarefa.classList.toggle('hidden')
-})
+});
 
 formAdicionarTarefa.addEventListener('submit', (evento) => {
     evento.preventDefault(); // ele vai prevenir o padrão, que no caso é recarregar a página quando o usuario der submit no form
@@ -17,5 +66,33 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
         descricao : textArea.value
     }
     tarefas.push(tarefa);
-    localStorage.setItem('tarefas', tarefas) //guarda a lista de tarefas dentro de um localStorage
-})
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
+    atualizarTarefas() //guarda a lista de tarefas dentro de um localStorage
+
+    // apagar o textArea e o form sumir
+    textArea.value = ''
+    formAdicionarTarefa.classList.add('hidden');
+});
+
+tarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
+});
+/*
+<li class="app__section-task-list-item">
+    <svg>
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>
+    </svg>
+    <p class="app__section-task-list-item-description">
+        Estudando localStorage
+    </p>
+    <button class="app_button-edit">
+        <img src="/imagens/edit.png">
+    </button>
+</li>
+*/
+
